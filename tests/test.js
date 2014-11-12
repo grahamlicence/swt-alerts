9a1b41d2fff022a00ab62fb12bbde1c6b00c43bb
Graham Licence <git@grahamlicence.co.uk>
2014-11-12 15:52:48 +0000

    Adding tests

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,150 @@
-describe("Status", function() {
-    var issues,
-        message = 'Good Service';
+// What are the messages?
+// SWT.service.notification     different coaches or started from different station
+// SWT.service.delays           any train delay
+// SWT.service.cancellations    any cancellation
+// SWT.updates.general          Adverse Weather Timetable/We will run a normal weekday service/Customer Information Systems fault
+// SWT.updates.line             Line problem/problem at a level crossing
+
+
+describe("Preferences buttons", function() {
+    SWT.preferences.open = false;
+
+    it("clicking the edit button should show the preferences form", function() {
+        SWT.preferences.toggle();
+        expect(SWT.preferences.open).toBe(true);
+    });
+
+    it("clicking the close button should hide the form", function() {
+        SWT.preferences.toggle();
+        expect(SWT.preferences.open).toBe(false);
+    });
+
+});
+
+describe("Status title", function() {
+    var message = 'Good Service';
 
     it("should show Good service if no issues", function() {
-        issues = 0;
+        SWT.service.notification = false;
+        SWT.service.delays = false;
+        SWT.service.cancellations = false;
+        SWT.updates.general = false;
+        SWT.updates.line = false;
+        SWT.Status.title();
         expect(SWT.title).toEqual(message);
     });
+
+    it("show Service Change if updates.general or service.notification and no other issues", function() {
+        message = 'Service Change';
+        SWT.service.notification = true;
+        SWT.service.delays = false;
+        SWT.service.cancellations = false;
+        SWT.updates.general = false;
+        SWT.updates.line = false;
+        SWT.Status.title();
+        expect(SWT.title).toEqual(message);
+
+        SWT.service.notification = false;
+        SWT.service.delays = false;
+        SWT.service.cancellations = false;
+        SWT.updates.general = true;
+        SWT.updates.line = false;
+        SWT.Status.title();
+        expect(SWT.title).toEqual(message);
+    });
+
+    it("show Trains Delayed if service.delays and no other issues", function() {
+        message = 'Trains Delayed';
+        SWT.service.notification = false;
+        SWT.service.delays = true;
+        SWT.service.cancellations = false;
+        SWT.updates.general = false;
+        SWT.updates.line = false;
+        SWT.Status.title();
+        expect(SWT.title).toEqual(message);
+    });
+
+    it("show Trains Delayed if service.delays and service.notification but no other issues", function() {
+        message = 'Trains Delayed';
+        SWT.service.notification = true;
+        SWT.service.delays = true;
+        SWT.service.cancellations = false;
+        SWT.updates.general = false;
+        SWT.updates.line = false;
+        SWT.Status.title();
+        expect(SWT.title).toEqual(message);
+    });
+
+    it("show Trains Cancelled if service.cancellations and no other issues", function() {
+        message = 'Trains Cancelled';
+        SWT.service.notification = false;
+        SWT.service.delays = false;
+        SWT.service.cancellations = true;
+        SWT.updates.general = false;
+        SWT.updates.line = false;
+        SWT.Status.title();
+        expect(SWT.title).toEqual(message);
+    });
+
+    it("show Trains Cancelled if service.cancellations and any combination of service.notification/service.delays and no other issues", function() {
+        message = 'Trains Cancelled';
+        SWT.service.notification = true;
+        SWT.service.delays = true;
+        SWT.service.cancellations = true;
+        SWT.updates.general = false;
+        SWT.updates.line = false;
+        SWT.Status.title();
+        expect(SWT.title).toEqual(message);
+
+        SWT.service.notification = false;
+        SWT.service.delays = true;
+        SWT.Status.title();
+        expect(SWT.title).toEqual(message);
+
+        SWT.service.notification = true;
+        SWT.service.delays = false;
+        SWT.Status.title();
+        expect(SWT.title).toEqual(message);
+    });
+
+    it("show Line blocked if updates.line and no other issues", function() {
+        message = 'Line Blocked';
+        SWT.service.notification = false;
+        SWT.service.delays = false;
+        SWT.service.cancellations = false;
+        SWT.updates.general = false;
+        SWT.updates.line = true;
+        SWT.Status.title();
+        expect(SWT.title).toEqual(message);
+    });
+
+    it("show Line blocked if updates.line and any combination of service.notification/service.cancellations/service.delays and no other issues", function() {
+        message = 'Line Blocked';
+        SWT.service.notification = true;
+        SWT.service.delays = false;
+        SWT.service.cancellations = false;
+        SWT.updates.general = false;
+        SWT.updates.line = true;
+        SWT.Status.title();
+        expect(SWT.title).toEqual(message);
+
+        SWT.service.notification = false;
+        SWT.service.delays = true;
+        SWT.service.cancellations = false;
+        SWT.Status.title();
+        expect(SWT.title).toEqual(message);
+
+        SWT.service.delays = false;
+        SWT.service.cancellations = true;
+        SWT.Status.title();
+        expect(SWT.title).toEqual(message);
+    });
+
 });
+
+
+
+
+
+
+
--- tests/tests.md
+++ tests/tests.md
@@ -4,13 +4,44 @@ DOM tests
 clicking the edit button should show the preferences form
 clicking the close button should hide the form
 
-Form tests
-clicking the clear button should clear the input
-entering text into the input should show station list
-saving the form should store form data in localstorage
-switch direction should switch the from and to station names
+Status type
+If category 'Station Updates' stationUpdates should be more than 0
+If title contains 'Line problem' or decription contains 'problem at a level crossing' updates.line should be more than 0
+If description contains 'We will run a normal weekday service' or 'Customer Information Systems fault' updates.general should be more than 0
+If description contains 'Will be formed of' serviceUpdates and service.notification should be more than 0
+If description contains 'will be started' serviceUpdates and service.notification should be more than 0
+If description contains 'This train has been delayed' serviceUpdates and service.delays should be more than 0
+If description contains 'no longer call' serviceUpdates and service.delays should be more than 0
+If title contains 'Delays' serviceUpdates and service.delays should be more than 0
+If description contains 'will be cancelled' serviceUpdates and service.cancelled should be more than 0
+If description contains 'will be terminated' serviceUpdates and service.cancelled should be more than 0
 
-Status messages
+
+Status title
 Show Good Service if no other issues
-Show Short Service if train length shorter and no other issues
-Show Trains Cancelled if cancellations
+Show Service Change if updates.general or service.notification and no other issues
+Show Trains Delayed if service.delays and no other issues
+Show Trains Delayed if service.delays and service.notification but no other issues
+Show Trains Cancelled if service.cancellations and no other issues
+Show Trains Cancelled if service.cancellations and any combination of service.notification/service.delays and no other issues
+Show Line blocked if updates.line and no other issues
+Show Line blocked if updates.line and any combination of service.notification/service.cancellations/service.delays and no other issues
+
+Status icon
+Show green is service good
+Show yellow if short formation/notification
+Show orange if delays
+Show red if cancellations or line problems
+
+Status message
+Show 'No issues' if no service updates
+Show 'No issues' if no general updates
+Show 'No issues' if no line updates
+Show 'No issues' if no station updates
+Show 'No issues' if no tube updates
+Show 'No updates set' if no tube updates in settings
+Show issues if service updates
+Show issues if general updates
+Show issues if line updates
+Show issues if station updates
+Show issues if tube updates
